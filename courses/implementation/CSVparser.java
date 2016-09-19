7fbeff460bf8a5888c102e6b1613162e18ca4943
southkid23 <aldo_aaa123@hotmail.com>
2016-09-19 15:47:36 -0500

    added CSVparser.java file and prequisites.csv file

---
+++ courses/implementation/CSVparser.java
@@ -0,0 +1,22 @@
+package implementation;
+
+public class CSVparser {
+
+	public static String[] parseClass(String class) {
+		// Will parse the csv file, using the "class" argument
+		// to get an array of strings that contains the students
+		// currently taking that class
+	}
+
+	public static String[] parseStudent(String studentID) {
+		// Will parse the csv file, using the "studentID" argument
+		// to get an array of strings that contains the classes
+		// taken by this student
+	}
+
+	public static String[] parsePreReq(String class) {
+		// Will parse the csv file, using the "class" argument
+		// to get an array of strings that contains the prerequisites
+		// of the class
+	}
+}
