69a2af71b7fe5fef9164475895cf5f94422ad63b
nxw13a <nxw13a@acu.edu>
2016-09-26 22:11:16 -0500

    small changes for you guys testing purpose

--- courses/implementation/Test.java
+++ courses/implementation/Test.java
@@ -16,7 +16,7 @@ class Test {
 
 		//uncommted below to see all the list
 		//for(int i = 400; i < 500;i++)
-			//System.out.println(someList2.get(i));
+		System.out.println(someList2);
 		System.out.println(someList2.size());
 	}
 
