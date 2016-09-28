bef3cce999700e7f6c2a24e895dc9caae96acadd
nxw13a <nxw13a@acu.edu>
2016-09-28 08:56:35 -0500

    few change

--- courses/implementation/Test.java
+++ courses/implementation/Test.java
@@ -15,17 +15,17 @@ class Test {
 		List<String> someList1 = new ArrayList<String>(csvParser.parseUnique(5,6));
 		List<String> someList2 = new ArrayList<String>(csvParser.parseClass(course,semester));
 		List<String> someList3 = new ArrayList<String>(csvParser.parseStudent(student_name));
-		/*
-		System.out.println(student_name + "took:\n");
+
+		System.out.println(student_name + " took:\n");
 		for(int i = 0; i < someList3.size();i++)
 			System.out.println(someList3.get(i));
-		*/
-			
-		/*
+	
+		System.out.println("\n");
+		
 		System.out.println("Find " + course + " with " + semester + ":\n");
 		for(int i = 0; i < someList2.size();i++)
 			System.out.println(someList2.get(i));
-		*/
+	
 		
 		/*
 		System.out.println("Distinct Courses:\n");
