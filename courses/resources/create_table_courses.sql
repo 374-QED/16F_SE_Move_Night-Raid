c0d40e569af50245f8aafc678a98f9cf5f35230e
Dcb426 <Dcb13a@acu.edu>
2016-10-12 13:46:13 -0500

    Demo_3

--- courses/resources/create_table_courses.sql
+++ courses/resources/create_table_courses.sql
@@ -1,12 +1,1 @@
-create table cs374_anon (
-Pidm varchar(20),
-Term_Code varchar(20),
-Reg_STS_Code varchar(10),
-Subject_Code varchar(10),
-Course_number varchar(20),
-Grade_Code varchar(225),
-Banner_id varchar(20),
-First_name varchar(225),
-Last_name varchar(225),
-Middle_name varchar(225)
-);
+ 
