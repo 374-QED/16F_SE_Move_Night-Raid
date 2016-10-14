87abbcc4c040841819ea16c34a5f5088a00a6687
nxw13a <nxw13a@acu.edu>
2016-10-14 12:55:23 -0500

    Change test

--- courses/implementation/Test.java
+++ courses/implementation/Test.java
@@ -12,9 +12,14 @@ class Test {
 		String semester;
 		AccessSQL_CSV test = new AccessSQL_CSV();
 		Scanner user_input = new Scanner(System.in);
-		System.out.println("Enter course: ");
+		System.out.println("Team: Night-Raid");
+		System.out.println("Members: Aldo Anaya, Nattapat White, and Darius Bell");
+		System.out.println("Project for CS374.01 Software Engineering");
+		System.out.println("Date: 10/14/2016");
+		System.out.println("\n");
+		System.out.println("Enter a course (such as 'CS120'): ");
 		course = user_input.next();
-		System.out.println("Enter semester: ");
+		System.out.println("Enter a semester (such as '201320'): ");
 		semester = user_input.next();
 		test.print(course,semester);
 
