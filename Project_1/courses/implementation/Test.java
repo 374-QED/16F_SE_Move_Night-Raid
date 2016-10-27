0e43ab5df5397b252260f5085269cbc0bb4c5877
shawn yap <scy12a@acu.edu>
2016-10-27 13:11:21 -0500

    Fixed compile permission

--- Project_1/courses/implementation/Test.java
+++ Project_1/courses/implementation/Test.java
@@ -8,7 +8,6 @@ class Test {
 
 	public static void main(String[] args) throws Exception
 	{
-		/*
 		String course;
 		String semester;
 		AccessSQL_CSV test = new AccessSQL_CSV();
@@ -23,15 +22,6 @@ class Test {
 		System.out.println("Enter a semester (such as '201320'): ");
 		semester = user_input.next();
 		test.print(course,semester);
-		*/
-		for(int x = 0; x < 10; x++)
-		{
-			if(x % 2 == 0)
-				System.out.println("Hello, World");
-			else
-				System.out.println("Hello, Universe");
-		}
-		
 	}	
 
 }
