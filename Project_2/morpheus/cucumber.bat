1d9771069a601a67ebbad288613627d55bbdb7e4
nxw13a <nxw13a@acu.edu>
2016-11-02 13:48:56 -0500

    add test case

--- Project_2/morpheus/cucumber.bat
+++ Project_2/morpheus/cucumber.bat
@@ -1,2 +1,2 @@
-javac -cp "jars/*" step_definitions/Morpheus_steps.java
-java -cp "jars/*;." cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features 
+javac -cp "jars/*;implementation/*;." step_definitions/Morpheus_steps.java
+java -cp "jars/*;.;implementation/*" cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features 
--- Project_2/morpheus/step_definitions/Morpheus_steps.java
+++ Project_2/morpheus/step_definitions/Morpheus_steps.java
@@ -2,8 +2,11 @@ package step_definitions;
 import cucumber.api.java.en.*;
 import cucumber.api.PendingException;
 import static org.junit.Assert.*;
+import implementation.SQLiteAccess;
+import java.sql.ResultSet;
 
 public class Morpheus_steps {
+	static SQLiteAccess test = new SQLiteAccess();
 
 	
 	@Given("^the days \"([^\"]*)\" and the course \"([^\"]*)\"$")
@@ -14,30 +17,37 @@ public class Morpheus_steps {
 
 	@Then("^the course can be moved to \"([^\"]*)\"$")
 	public void theCourseCanBeMovedTo(String arg1) throws Throwable {
-	
+		ResultSet rs = test.readDatabase("select * from classes limit 10");
+	    String target = test.writeString(rs, "Banner_ID");
+	    assertEquals(target, target);
 	}
 
 	@Given("^the days \"([^\"]*)\" and start time \"([^\"]*)\"$")
 	public void theDaysAndStartTime(String arg1, String arg2) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    //throw new PendingException();
+	   
+
 	}
 
 	@Then("^\"([^\"]*)\" has a class at that time$")
 	public void hasAClassAtThatTime(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    ResultSet rs = test.readDatabase("select * from classes limit 10");
+	    String target = test.writeString(rs, "Banner_ID");
+	    assertEquals(target, target);
 	}
 
 	@Given("^the course \"([^\"]*)\"$")
 	public void theCourse(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@Then("^the max enrolled is \"([^\"]*)\"\\.$")
 	public void theMaxEnrolledIs(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    ResultSet rs = test.readDatabase("select * from classes limit 10");
+	    String target = test.writeString(rs, "Banner_ID");
+	    assertEquals(target, target);
 	}
 }
