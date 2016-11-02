d3e585cf3334cf17e28fd95c48c98c99b7cc4fa6
southkid23 <aldo_aaa123@hotmail.com>
2016-11-02 12:45:39 -0500

    updated features and more methods

--- Project_2/morpheus/step_definitions/Morpheus_steps.java
+++ Project_2/morpheus/step_definitions/Morpheus_steps.java
@@ -5,11 +5,7 @@ import static org.junit.Assert.*;
 
 public class Morpheus_steps {
 
-	@Before public void setUp() {
-
-		CSV_parser parser = new CSV_parser();
-	}
-
+	
 	@Given("^the days \"([^\"]*)\" and the course \"([^\"]*)\"$")
 	public void theDaysAndTheCourse(String arg1, String arg2) throws Throwable {
 	
@@ -19,5 +15,29 @@ public class Morpheus_steps {
 	@Then("^the course can be moved to \"([^\"]*)\"$")
 	public void theCourseCanBeMovedTo(String arg1) throws Throwable {
 	
+	}
+
+	@Given("^the days \"([^\"]*)\" and start time \"([^\"]*)\"$")
+	public void theDaysAndStartTime(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^\"([^\"]*)\" has a class at that time$")
+	public void hasAClassAtThatTime(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^the course \"([^\"]*)\"$")
+	public void theCourse(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the max enrolled is \"([^\"]*)\"\\.$")
+	public void theMaxEnrolledIs(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
 	}
 }
