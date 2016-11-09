54e034c2e017eb7cc47d62824d0718e6c4eedc06
southkid23 <aldo_aaa123@hotmail.com>
2016-11-09 12:46:30 -0600

    forgot to save

--- Project_2/morpheus/implementation/ClassMove.java
+++ Project_2/morpheus/implementation/ClassMove.java
@@ -12,11 +12,23 @@ class ClassMove {
 
 		SQLiteAccess sqlite = new SQLiteAccess();
 
+		System.out.println("\nInput the class you would like to move.  -- Example: \"CS120\"");
+		System.out.println(sqlite.readDatabase())
 		Scanner user_input = new Scanner(System.in);
 		String class;
 
-		System.out.println("Which class would you like to move?  Example: \"CS120\"");
-		class = user_input.next();
+		List<int> studentTimeMWF = // 	give me a list that only contains the distinct begin times
+									// 	of the students' other classes for MWF
+		
+		List<int> studentTimeTR = // 	give me a list that only contains the distinct begin times
+									// 	of the students' other classes for TR
+
+		System.out.println("Do you want to move the class to the ?");
+		Scanner user_input = new Scanner(System.in);
+		String class;
+
+
+		ResultSet 
 	}
 
 }
