07273ea1882471a78ecd6bb55d49150d6e5741e2
southkid23 <aldo_aaa123@hotmail.com>
2016-11-28 13:51:23 -0600

    Merged changes

--- Project_2/morpheus/test.bat
+++ Project_2/morpheus/test.bat
@@ -1,5 +1,5 @@
 @echo off
 
-javac -cp "jars/*;.;resources/SQLite/*" implementation\ClassMove.java implementation\SQLiteAccess.java implementation\nrLib.java
+javac -cp "jars/*;.;resources/SQLite/*" implementation\Test.java implementation\SQLiteAccess.java implementation\nrLib.java
 
-if %errorlevel%==0 java -cp ".;jars/*" implementation.ClassMove
+if %errorlevel%==0 java -cp ".;jars/*" implementation.Test
