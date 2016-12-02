3525edafa976ae0dab8858f0daffe688d8761e93
nxw13a <nxw13a@acu.edu>
2016-12-01 20:00:57 -0600

    add Complexity and MacOS GUI

--- Project_2/morpheus/runGUI.bat
+++ Project_2/morpheus/runGUI.bat
@@ -1,5 +1,4 @@
 @echo off
 
 javac -cp "jars/*;.;resources/SQLite/*" implementation\GUI.java implementation\GUI2.java implementation\GUI3.java implementation\GUI4.java implementation\SQLiteAccess.java implementation\nrLib.java
-
-if %errorlevel%==0 java -cp ".;jars/*" implementation.GUI
+java -cp ".;jars/*" implementation.GUI
